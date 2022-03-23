b962121546c2a3d6684ac046b4c692a8b110e64c
05arnab05 <arnabch5991@gmail.com>
2022-03-23 08:40:36 +0200

    New created plotting file

---
+++ history.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+
+"""
+import matplotlib.pyplot as plt
+x=[1,2,3,4,5,6]
+y=[5,3,4,6,7,8]
+
+def createPlots():
+ plt.barh(x,y)
+ plt.show()
+ 
+def main():
+    createPlots()
+    createPlots()
+
+if __name__ == "__main__":
+    main()
